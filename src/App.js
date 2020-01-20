1cf966d834b00e7311d78e8e6248f6acb2e5aceb
Reina <r.clarkemitchell@gmail.com>
2020-01-20 03:42:02 -0500

    go to sleep

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
-import NavBar from './layout/NavBar'
+import NavBar from './Components/layout/NavBar'
+import UserDetails from './Components/Users/UserDetails'
 
 export default class App extends Component {
 	render() {
 		return (
 			<div className='App'>
 				<NavBar />
+				<UserDetails />
 			</div>
 		)
 	}

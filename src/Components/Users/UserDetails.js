7e4aa60c67f84bf06651b4311a11f5071ae4601c
Reina <r.clarkemitchell@gmail.com>
2020-01-20 03:52:00 -0500

    added target_blank

--- src/Components/Users/UserDetails.js
+++ src/Components/Users/UserDetails.js
@@ -21,7 +21,12 @@ export default class UserDetails extends Component {
 				></img>
 				<h3>{login}</h3>
 				<div>
-					<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					<a
+						href={html_url}
+						target='_blank'
+						rel='noopener noreferrer'
+						className='btn btn-dark btn-sm my-1'
+					>
 						More
 					</a>
 				</div>
